1f0a1d81d3abefddf9cb881a1d3349357c41c379
PhucMangue <126844674+PhucMangue@users.noreply.github.com>
2023-03-27 12:22:33 +0700

    upload

--- main.js
+++ main.js
@@ -33,6 +33,8 @@ menuBtn.addEventListener('click', showMenu)
 closeBtn.addEventListener('click',closeMenu)
 
 // end menu
+
+// menu cap 2
 const list1=document.querySelector('.js-list-1');
 const list2=document.querySelector('.js-list-2');
 const list3=document.querySelector('.js-list-3');
@@ -41,14 +43,31 @@ const showLs1=document.querySelector('.ls1');
 const showLs2=document.querySelector('.ls2');
 const showLs3=document.querySelector('.ls3');
 function showList1(){
-    showLs1.classList.add('open');
+    showLs1.classList.toggle('open');
 }
 function showList2(){
-  showLs2.classList.add('open');
+  showLs2.classList.toggle('open');
 }
 function showList3(){
-  showLs3.classList.add('open');
+  showLs3.classList.toggle('open');
 }
 list1.addEventListener('click',showList1)
 list2.addEventListener('click',showList2)
 list3.addEventListener('click',showList3)
+// end menu cap 2
+
+// search 
+const timkiem=document.querySelector('#search-mobile');
+const modalSearch=document.querySelector('.modal-search');
+const searchContainer=document.querySelector('.modal-search-container');
+function openSearch(){
+  modalSearch.classList.add('open');
+}
+modalSearch.addEventListener('click',function(close){
+  modalSearch.classList.remove('open');
+})
+timkiem.addEventListener('click',openSearch) 
+searchContainer.addEventListener('click',function(event){
+  event.stopPropagation()
+})
+
